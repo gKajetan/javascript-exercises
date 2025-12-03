57aa0804eb46f914ce12be18950b339b2e901bed
Kajetan <147710182+gKajetan@users.noreply.github.com>
2025-12-04 00:15:48 +0100

    11_tempConversion completed
    Tests passed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let calculated = (temp-32)*5/9;
+  let calculatedRounded = Math.round(calculated * 10) / 10;
+  return calculatedRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let calculated = (temp*9/5)+32;
+  let calculatedRounded = Math.round(calculated * 10) / 10;
+  return calculatedRounded;
 };
 
 // Do not edit below this line
